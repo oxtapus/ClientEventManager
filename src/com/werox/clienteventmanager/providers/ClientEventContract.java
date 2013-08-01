f89af30e908f209fd9f0d94a33d149cffba5368b
oxtapus <a@b.c>
2013-08-01 09:03:40 +0100

    Initial commit
    Does not build

---
+++ src/com/werox/clienteventmanager/providers/ClientEventContract.java
@@ -0,0 +1,126 @@
+package com.werox.clienteventmanager.providers;
+
+public class ClientEventContract {
+
+    /**
+     * Special value for {@link SyncColumns#UPDATED} indicating that an entry
+     * has never been updated, or doesn't exist yet.
+     */
+    public static final long UPDATED_NEVER = -2;
+
+    /**
+     * Special value for {@link SyncColumns#UPDATED} indicating that the last
+     * update time is unknown, usually when inserted from a local file source.
+     */
+    public static final long UPDATED_UNKNOWN = -1;
+
+    public interface SyncColumns {
+        /** Last time this entry was updated or synchronised. */
+        String UPDATED = "updated";
+    }
+	
+	interface ClientColumns {
+        /** Unique string identifying this client. */
+        String CLIENT_ID = "client_id";
+        /** The clients title. */
+        String TITLE = "client_title";
+        /** The clients first name. */
+        String NAME = "client_name";
+        /** The clients phone number. */
+        String PHONE = "client_phone";
+        /** The clients mobile phone number. */
+        String MOBILE_PHONE = "client_mobile_phone";
+        /** The clients email address. */
+        String EMAIL = "client_email";
+        /** The last date the client had a booking. */
+        String LAST_BOOKING_DATE = "client_last_booking_date";
+    }
+	
+	interface EventTypeColumns {
+        /** Unique string identifying this event type. */
+        String EVENT_TYPE_ID = "event_type_id";
+        /** The event types first name. */
+        String NAME = "event_type_name";
+        /** The maximum number of attendees that can attend the event. */
+        String MAXIMUM_ATTENDEES = "event_type_maximum_attendees";
+        /** The cost of the event. */
+        String COST = "event_type_cost";
+        /** The duration of the event in hours. */
+        String DURATION_HOURS = "event_type_duration_hours";
+    }
+	
+	interface EventColumns {
+        /** Unique string identifying this event. */
+        String EVENT_ID = "event_id";
+        /** The id of the event types the event links to. */
+        String EVENT_TYPE_ID = "event_type_id";
+        /** The location id of the event. */
+        String LOCATION_ID = "event_location_id";
+        /** The start time of the event. */
+        String START_TIME = "event_start_time";
+        /** The end time of the event. */
+        String END_TIME = "event_end_time";
+        /** The cost of the event. */
+        String COST = "event_cost";
+        /** The cost of the event. */
+        String ATTENDEE_COUNT = "event_attendee_count";
+    }
+	
+	interface EventAttendeeColumns {
+        /** The id of the event. */
+        String EVENT_ID = "event_attendee_event_id";
+        /** The id of the client attending the event. */
+        String CLIENT_ID = "event_attendee_client_id";
+        /** The status of the payment(s) made by the client to attend the event. */
+        String PAYMENT_STATUS_ID = "event_attendee_payment_status_id";
+        /** The total payment made by the client. */
+        String CLIENT_PAYMENT_AMMOUNT = "event_attendee_client_payment_ammount";
+    }
+	
+	interface LocationColumns {
+        /** The id of the location. */
+        String LOCATION_ID = "location_id";
+        /** The name of the location. */
+        String NAME = "location_name";
+        /** The address of the location. */
+        String ADDRESS = "location_address";
+    }
+	
+	interface ReferralColumns {
+        /** The id of the referral. */
+        String REFERRAL_ID = "referral_id";
+        /** The status id of the referral. */
+        String REFERRAL_STATUS_ID = "referral_status_id";
+        /** The id of the client who made the referral. */
+        String REFERRING_CLIENT_ID = "referral_referring_client_id";
+        /** The client id of the client who was referred. */
+        String REFERRED_CLIENT_ID = "referral_referred_client_id";
+        /** The credit value of the referral. */
+        String CREDIT_VALUE = "referral_credit_calue";
+    }
+	
+	interface ReferralStatusColumns {
+        /** The id of the referral status. */
+        String REFERRAL_STATUS_ID = "referral_status_id";
+        /** The text of the referral status. */
+        String TEXT = "referral_status_text";
+    }
+	
+	interface PaymentStatusColumns {
+        /** The id of the payment status. */
+        String PAYMENT_STATUS_ID = "payment_status_id";
+        /** The text of the payment status. */
+        String TEXT = "payment_status_text";
+    }
+	
+	interface ClientCreditColumns {
+        /** The id of the client credit. */
+        String CLIENT_CREDIT_ID = "client_credit_id";
+        /** The client id. */
+        String CLIENT_ID = "client_credit_client_text";
+        /** The value of the credit. */
+        String CREDIT_VALUE = "client_credit_value";
+        /** Value to store whether the credit has been claimed. */
+        String CLAIMED = "client_credit_claimed";
+    }
+}
---
+++ src/com/werox/clienteventmanager/providers/ClientEventDatabase.java
@@ -0,0 +1,171 @@
+package com.werox.clienteventmanager.providers;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.provider.CalendarContract.Events;
+
+import com.werox.clienteventmanager.providers.ClientEventContract.ClientColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.ClientCreditColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.EventAttendeeColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.EventColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.EventTypeColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.LocationColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.PaymentStatusColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.ReferralColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.ReferralStatusColumns;
+import com.werox.clienteventmanager.providers.ClientEventContract.SyncColumns;
+
+public class ClientEventDatabase extends SQLiteOpenHelper {
+	
+    private static final String TAG = makeLogTag(ClientEventDatabase.class);
+
+    private static final String DATABASE_NAME = "clientevent.db";
+
+    private static final int VER_LAUNCH = 1;
+    private static final int VER_SESSION_TYPE = 1;
+
+    private static final int DATABASE_VERSION = VER_SESSION_TYPE;
+    
+    interface Tables {
+        String CLIENTS = "clients";
+        String EVENT_TYPE = "event_type";
+        String EVENTS = "events";
+        String EVENT_ATTENDEES = "event_attendees";
+        String LOCATIONS = "locations";
+        String REFERRALS = "referrals";
+        String REFERRAL_STATUS = "referral_status";
+        String PAYMENT_STATUS = "payment_status";
+        String CLIENT_CREDITS = "client_credits";
+
+        String SESSIONS_SEARCH = "sessions_search";
+
+        String SEARCH_SUGGEST = "search_suggest";
+
+        String EVENTS_JOIN_EVENTS_TYPE = "events "
+                + "LEFT OUTER JOIN events_type ON events.event_type_id=event_type.event_type_id";
+
+        String EVENTS_JOIN_LOCATIONS = "events "
+                + "LEFT OUTER JOIN locations ON events.event_location_id=locations.location_id";
+
+        String EVENT_ATTENDEES_JOIN_EVENTS = "events "
+                + "LEFT OUTER JOIN event_attendees ON events.event_id=event_attendees.event_attendee_event_id";
+
+        String EVENT_ATTENDEES_JOIN_CLIENTS = "clients "
+                + "LEFT OUTER JOIN event_attendees ON clients.client_id=event_attendees.client_id";
+
+        String EVENT_ATTENDEES_JOIN_PAYMENT_STATUS = "payment_status "
+                + "LEFT OUTER JOIN event_attendees ON payment_status.payment_status_id=event_attendees.payment_status_id";
+
+        String REFERRING_CLIENT_JOIN_REFERRALS = "clients "
+                + "LEFT OUTER JOIN referal_status ON referrals.referring_client_id=clients.client_id";
+        
+        String REFERRED_CLIENT_JOIN_REFERRALS = "clients "
+                + "LEFT OUTER JOIN referal_status ON referrals.referred_client_id=clients.client_id";
+
+        String REFERRAL_STATUS_JOIN_REFERRALS = "referrals "
+                + "LEFT OUTER JOIN referal_status ON referrals.referral_status_id=referal_status.referral_status_id";
+
+        String CLIENT_CREDITS_JOIN_CLIENTS = "clients "
+                + "LEFT OUTER JOIN client_credits ON clients.client_id=client_credits.client_id";
+    }
+
+    /** {@code REFERENCES} clauses. */
+    private interface References {
+        String CLIENT_ID = "REFERENCES " + Tables.CLIENTS + "(" + Clients.CLIENT_ID + ")";
+        String EVENT_TYPE_ID = "REFERENCES " + Tables.EVENT_TYPE + "(" + Event_Type.EVENT_TYPE_ID + ")";
+        String EVENT_ID = "REFERENCES " + Tables.EVENTS + "(" + Events.EVENT_ID + ")";
+        String EVENT_ATTENDEES_ID = "REFERENCES " + Tables.EVENT_ATTENDEES + "(" + Event_Attendees.EVENT_ATTENDEES_ID + ")";
+        String LOCATION_ID = "REFERENCES " + Tables.LOCATIONS + "(" + Locations.LOCATION_ID + ")";
+        String REFERRAL_ID = "REFERENCES " + Tables.REFERRALS + "(" + Referrals.REFERRAL_ID + ")";
+        String REFERRAL_STATUS_ID = "REFERENCES " + Tables.REFERRAL_STATUS + "(" + Referral_Status.REFERRAL_STATUS_ID + ")";
+        String REFERRING_CLIENT_ID = "REFERENCES " + Tables.REFERRAL_STATUS + "(" + Referral_Status.REFERRING_CLIENT_ID + ")";
+        String REFERRED_CLIENT_ID = "REFERENCES " + Tables.REFERRAL_STATUS + "(" + Referral_Status.REFERRED_CLIENT_ID + ")";
+        String PAYMENT_STATUS_ID = "REFERENCES " + Tables.PAYMENT_STATUS + "(" + Payment_Status.PAYMENT_STATUS_ID + ")";
+        String CLIENT_CREDIT_ID = "REFERENCES " + Tables.CLIENT_CREDITS + "(" + Client_Credits.CLIENT_CREDIT_ID + ")";
+    }
+    
+    public ClientEventDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + Tables.CLIENTS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + ClientColumns.TITLE + " TEXT,"
+                + ClientColumns.NAME + " TEXT NOT NULL,"
+                + ClientColumns.PHONE + " TEXT,"
+                + ClientColumns.MOBILE_PHONE + " TEXT,"
+                + ClientColumns.EMAIL + " TEXT,"
+                + ClientColumns.LAST_BOOKING_DATE + " INTEGER,"
+                + "UNIQUE (" + ClientColumns.CLIENT_ID + ") ON CONFLICT REPLACE)");
+
+        db.execSQL("CREATE TABLE " + Tables.EVENT_TYPE + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + EventTypeColumns.NAME + " TEXT NOT NULL,"
+                + EventTypeColumns.MAXIMUM_ATTENDEES + " INTEGER,"
+                + EventTypeColumns.COST + " INTEGER NO NULL,"
+                + EventTypeColumns.DURATION_HOURS + " INTEGER NOT NULL,"
+                + "UNIQUE (" + EventTypeColumns.EVENT_TYPE_ID + ") ON CONFLICT REPLACE)");
+
+        db.execSQL("CREATE TABLE " + Tables.EVENTS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + EventColumns.EVENT_TYPE_ID + " INTEGER " + References.EVENT_TYPE_ID + ","
+                + EventColumns.LOCATION_ID + " INTEGER " + References.LOCATION_ID + ","
+                + EventColumns.START_TIME + " INTEGER NOT NULL,"
+                + EventColumns.END_TIME + " INTEGER NOT NULL,"
+                + "UNIQUE (" + EventColumns.EVENT_ID + ") ON CONFLICT REPLACE)");
+
+        db.execSQL("CREATE TABLE " + Tables.EVENT_ATTENDEES + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + EventAttendeeColumns.EVENT_ID + " INTEGER NOT NULL " + References.EVENT_ID + ","
+                + EventAttendeeColumns.CLIENT_ID + " INTEGER NOT NULL " + References.CLIENT_ID + ","
+                + EventAttendeeColumns.PAYMENT_STATUS_ID + " INTEGER NOT NULL " + References.PAYMENT_STATUS_ID + ","
+                + EventAttendeeColumns.CLIENT_PAYMENT_AMMOUNT + " INTEGER NOT NULL "
+                + "UNIQUE (" + EventAttendeeColumns.EVENT_ID + ","
+                		+ EventAttendeeColumns.CLIENT_ID + ") ON CONFLICT REPLACE)");
+
+        db.execSQL("CREATE TABLE " + Tables.LOCATIONS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + LocationColumns.NAME + " TEXT NOT NULL,"
+                + LocationColumns.ADDRESS + " TEXT,"
+                + "UNIQUE (" + LocationColumns.LOCATION_ID + ") ON CONFLICT REPLACE)");
+
+        db.execSQL("CREATE TABLE " + Tables.REFERRALS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + ReferralColumns.REFERRAL_STATUS_ID + " INTEGER NOT NULL " + References.REFERRAL_STATUS_ID + ","
+                + ReferralColumns.REFERRING_CLIENT_ID + " INTEGER NOT NULL " + References.REFERRING_CLIENT_ID + ","
+                + ReferralColumns.REFERRED_CLIENT_ID + " INTEGER NOT NULL " + References.REFERRED_CLIENT_ID + ","
+                + ReferralColumns.CREDIT_VALUE + " INTEGER NOT NULL,"
+                + "UNIQUE (" + ReferralColumns.REFERRAL_ID + ") ON CONFLICT REPLACE)");
+        
+        db.execSQL("CREATE TABLE " + Tables.REFERRAL_STATUS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + ReferralStatusColumns.TEXT + " TEXT NOT NULL, "
+                + "UNIQUE (" + ReferralStatusColumns.REFERRAL_STATUS_ID + ") ON CONFLICT REPLACE)");
+        
+        db.execSQL("CREATE TABLE " + Tables.PAYMENT_STATUS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + PaymentStatusColumns.TEXT + " TEXT NOT NULL, "
+                + "UNIQUE (" + PaymentStatusColumns.PAYMENT_STATUS_ID + ") ON CONFLICT REPLACE)");
+
+        db.execSQL("CREATE TABLE " + Tables.CLIENT_CREDITS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SyncColumns.UPDATED + " INTEGER NOT NULL,"
+                + ClientCreditColumns.CLIENT_ID + " INTEGER NOT NULL " + References.CLIENT_ID + ","
+                + ClientCreditColumns.CREDIT_VALUE + " INTEGER NOT NULL,"
+                + ClientCreditColumns.CLAIMED + " INTEGER, "
+                + "UNIQUE (" + ClientCreditColumns.CLIENT_CREDIT_ID + ") ON CONFLICT REPLACE)");
+    }
+}
